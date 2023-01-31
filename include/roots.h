4c22ad98866466e8dd600722215e9b778a8f7bc7
Daniel Palacio <dpalacio@student.hive.fi>
2023-01-31 17:18:41 +0200

    rendering title screen

--- include/roots.h
+++ include/roots.h
@@ -2,7 +2,7 @@
 # define ROOTS_H
 
 # define WIN_NAME "Root"
-# define WIN_W 1960
+# define WIN_W 1920
 # define WIN_H 1080
 
 # define NAME               "ROOTS"
--- src/render.cpp
+++ src/render.cpp
@@ -9,8 +9,19 @@ void Engine::render()
     Win.draw(BackgroundSprite);
 
 	// Render title text
-	sf::Text text("ROOTS", font, 240);
-
+	sf::Text text;
+	text.setFont(font);
+	text.setString("ROOTS");
+	text.setCharacterSize(256);
+	text.setFillColor(sf::Color:: Black);
+	text.setPosition(Win.getSize().x * 0.5 - 128 * 3 +4,
+		Win.getSize().y * 0.5 - 252);
+	Win.draw(text);
+	text.setFillColor(sf::Color:: Green);
+	text.setOutlineColor(sf::Color:: Black);
+	text.setPosition(Win.getSize().x * 0.5 - 128 * 3,
+		Win.getSize().y * 0.5 - 256);
+	Win.draw(text);
     // Show everything we have just drawn
     Win.display();
 }
